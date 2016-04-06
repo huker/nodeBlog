a79c34ad770df3f331402e4a5acba6e81b8c4df3
huker <huaolie@sina.com>
2016-04-06 20:17:01 +0800

    addarticle
    发表文章 建立文章的数据库表

---
+++ model/article.js
@@ -0,0 +1,18 @@
+/*
+* 文章写进数据库
+* */
+var mongoose = require('mongoose');
+//mongoose.connect('mongodb://127.0.0.1:27017/nodeBlog');
+//定义模型 确定数据库里表结构
+var articleSchema = new mongoose.Schema({
+    title:String,
+    content:String,
+    //类型是主键类型 引用的是user
+    user:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
+    creatAt:{type:Date,default:Date.now}
+});
+
+//定义model
+var articleModel = mongoose.model('article',articleSchema);
+
+module.exports=articleModel;
--- routes/articles.js
+++ routes/articles.js
@@ -3,8 +3,9 @@
  */
 var express = require('express');
 var router=express.Router();
-
+var articleModel=require('../model/article.js');
 /*
+* 文章路由:
 * 请求一个空白文章的页面 get
 * 提交文章数据 post
 * */
@@ -14,6 +15,17 @@ router.get('/add',function(req,res){
 });
 
 router.post('/add',function(req,res){
-    res.send('article')
+    var article=req.body;
+    var user=req.session.user;
+    article.user=user;
+    articleModel.create(article,function(err,article){
+        if(err){
+            req.flash('error','文章发表失败');
+            res.redirect('back');
+        }else{
+            req.flash('success','文章发表成功');
+            res.redirect('/');
+        }
+    });
 });
 module.exports=router;
