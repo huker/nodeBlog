9d698bde3e6499afd0ee7a59a731de7b6a09aa0e
huker <huaolie@sina.com>
2017-04-18 00:19:09 +0800

    admin_remove_api

--- public/js/sysmain.js
+++ public/js/sysmain.js
@@ -1,113 +1,20 @@
 /**
- * Created by huk on 17/2/21.
+ * Created by huk on 17/4/15.
  */
 $(function () {
-
-    $('#searchBtn').click(function () {
-        var content = $('#s_content').val();
-        if(!content && content==''){
-            alert('请输入关键词')
-        }else{
-            window.location.href = '/search/' + content;
-        }
-
-        return false
-    });
-
-    $('.send_i').click(function () {
-        changeTextInput($(this))
-    });
-    $('#update_btn').click(function () {
-        window.location.href = '/articles/update/' + $(this).attr('data-type');
-    });
-    $('#center_nav_tips').on('click', 'li', function () {
-        $(this).addClass('active');
-        $(this).siblings().removeClass('active');
-        var tip = $(this).attr('data-type');
-        var uid = $('#userId').attr('data-type');
-        switch (tip) {
-            case 'article':
-                $('#tip_title').html('我的文章');
-                break;
-            case 'ask':
-                $('#tip_title').html('我的提问');
-                break;
-            case 'answer':
-                $('#tip_title').html('我的回答');
-                break;
-            case 'comment':
-                $('#tip_title').html('我的评论');
-                break;
-        }
-        center_tip_data(tip, uid);
-    })
-});
-/**
- * 点击修改图标文字变输入框
- * @param ele 当前ele
- */
-function center_tip_data(tip, id) {
-    $.ajax({
-        url: '/users/userCenter/' + tip + '/' + id,
-        method: 'GET',
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (data) {
-            $('#tip_list').empty();
-            if (data.length > 0) {
-                var html = '';
-                $.each(data, function (i, ele) {
-                    switch (tip) {
-                        case 'article':
-                            html += '<li class="list-group-item"><a href="/articles/detail/' + ele._id + '">' + ele.title + '</a></li>';
-                            break;
-                        case 'ask':
-                            html += '<li class="list-group-item"><a href="/questions/detail/' + ele._id + '">' + ele.title + '</a></li>';
-                            break;
-                        case 'answer':
-                            break;
-                        case 'comment':
-                            break;
-                    }
-                });
-                $('#tip_list').append(html);
-            }
-
-        },
-        error: function () {
-            $('#tip_list').html('无');
-        }
-    })
-}
-function changeTextInput(ele) {
-    var old_value = $(ele).prev().html();
-    var type = $(ele).attr('data-type');
-    $(ele).css('display', 'none');
-    $(ele).prev().css('display', 'none');
-    $(ele).after('<form action="" style="display: inline-block">' +
-        '<input type="text" id="changed_val" value="' + old_value + '">' +
-        '<button class="btn btn-sm btn-default change_btn">修改</button></form>');
-    $('.change_btn').click(function () {
-        sendNewValue(type);
-        return false
-    });
-    function sendNewValue(tip) {
-        var send_url = '/users/center/' + tip;
-        var data_str = {"value": $('#changed_val').val()};
-        var json = JSON.stringify(data_str);
-        console.log(json)
+    $('.delbtn').click(function (ev) {
+        var _id = $(ev.target).parent().parent().attr('data-type');
+        var type = $(ev.target).attr('data-url');
         $.ajax({
-            url: send_url,
-            type: 'POST',
-            data: json,
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (data) {
-                $(ele).css('display', 'inline');
-                $(ele).prev().css('display', 'inline-block');
-                $(ele).prev().html(data[tip]);
-                $(ele).next().remove();
+            url:'/admin/sys-'+type+'/'+_id,
+            method:'GET',
+            dataType:'json',
+            contentType:"application/json",
+            success:function (data) {
+                if(data.success){
+                    $(ev.target).parent().parent().remove();
+                }
             }
         })
-    }
-}
+    })
+});
--- routes/admin.js
+++ routes/admin.js
@@ -23,7 +23,7 @@ router.post('/login',function (req, res) {
                 req.session.user = user;
                 req.flash('success', '管理员登录成功');
                 // res.render('admin/manager',{user:user});
-                res.redirect('/admin/manageInfo')
+                res.redirect('/admin/manage-info')
             }else{
                 req.flash('error', '管理员登录失败');
                 res.redirect('back');
@@ -36,17 +36,54 @@ router.get('/manage-info',function (req,res) {
     var user = req.session.user;
     var system_user = [];
     userModel.find({},function (err, userData) {
-        userData.forEach(function (item) {
-            if(item.roles == 1){
-                system_user.push(item)
-            }
-        })
+        if(err){
+
+        }else{
+            userData.forEach(function (item) {
+                if(item.roles == 1){
+                    system_user.push(item)
+                }
+            })
+        }
     }).then(function () {
         console.log(system_user)
         res.render('admin/manager',{sys_user:system_user,user:user});
     })
 });
+router.get('/sys-user/:_id',function (req,res) {
+    var resdata = false;
+    userModel.remove({_id:req.params._id},function (err, result) {
+        if(err){
 
+        }else{
+            resdata = true;
+        }
+    }).then(function () {
+        //删除了用户后还需要删除它发表得到文章和问题
+        var $set = {user:{_id:req.params._id}};
+        articleModel.remove($set,function (err, result) {
+            if(err){
+
+            }else{
+                resdata = true;
+            }
+        })
+    }).then(function () {
+        var $setqs = {user:{_id:req.params._id}};
+        qsModel.remove($setqs,function (err, result) {
+            if(err){
+
+            }else{
+                resdata = true;
+            }
+        })
+    }).then(function () {
+        if(resdata){
+            var data = {"success":true};
+            res.end(JSON.stringify(data));
+        }
+    })
+});
 router.get('/sys-article',function (req,res) {
     var user = req.session.user;
     articleModel.find({}).populate('user').exec(function (err, articleData) {
@@ -57,18 +94,37 @@ router.get('/sys-article',function (req,res) {
         }
     })
 });
-
-router.get('/sys-qs',function (req,res) {
-    var user = req.session.user;
-    qsModel.find({},function (err, qsData) {
+router.get('/sys-article/:_id',function (req,res) {
+    articleModel.remove({_id:req.params._id},function (err, result) {
         if(err){
 
         }else{
-            res.render('admin/managerqs',{sys_article:qsData,user:user});
+            var resdata = {"success":true};
+            res.end(JSON.stringify(resdata));
         }
     })
 });
 
+router.get('/sys-qs',function (req,res) {
+    var user = req.session.user;
+    qsModel.find({}).populate('user').exec(function (err, qsData) {
+        if(err){
+
+        }else{
+            res.render('admin/managerqs',{sys_qs:qsData,user:user});
+        }
+    })
+});
+router.get('/sys-qs/:_id',function (req,res) {
+    qsModel.remove({_id:req.params._id},function (err, result) {
+        if(err){
+
+        }else{
+            var resdata = {"success":true};
+            res.end(JSON.stringify(resdata));
+        }
+    })
+});
 
 router.get('/logout',function (req, res) {
     req.session.user = null;
